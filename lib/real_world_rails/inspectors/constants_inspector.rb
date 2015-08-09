2b8b13e9a75b17e37807ee3327596d31f117031a
Eliot Sykes <eliotsykes@gmail.com>
2015-08-09 19:22:35 +0100

    Beginnings of ConstantsInspector

--- lib/real_world_rails/inspectors/constants_inspector.rb
+++ lib/real_world_rails/inspectors/constants_inspector.rb
@@ -3,9 +3,33 @@ module RealWorldRails
 
     class ConstantsInspector
       def run
-        raise NotImplementedError, "TODO: Write ConstantsInspector"
+        parser = ParserFactory.create
+        # files = Dir.glob("apps/**/*.rb")
+        files = Dir.glob("apps/24pullrequests/**/*.rb")
+        processor = Processor.new
+        files.each do |filename|
+          source = File.read(filename)
+          buffer = Parser::Source::Buffer.new filename
+          buffer.source = source
+          parser.reset
+          ast = parser.parse(buffer)
+          puts filename
+          processor.process(ast)
+        end
       end
     end
-    
+
+    class Processor < Parser::AST::Processor
+      def on_const(node)
+        # require 'pry'
+        # require 'pp'
+        # binding.pry
+        # class_name = node.children.first.children.last
+        if node.children.last =~ /^[A-Z]+$/
+          puts node.inspect
+        end
+      end
+    end
+
   end
 end
