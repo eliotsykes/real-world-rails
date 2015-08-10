22d009b2a68547723a9366f46e232fc4817cb1b0
Eliot Sykes <eliotsykes@gmail.com>
2015-08-10 12:31:23 +0100

    Refine constants inspector to use on_casgn (on constant assignment) and print source code

--- lib/real_world_rails/inspectors/constants_inspector.rb
+++ lib/real_world_rails/inspectors/constants_inspector.rb
@@ -5,30 +5,27 @@ module RealWorldRails
       def run
         parser = ParserFactory.create
         # files = Dir.glob("apps/**/*.rb")
-        files = Dir.glob("apps/24pullrequests/**/*.rb")
+        files = Dir.glob("apps/24pullrequests/app/**/*.rb")
         processor = Processor.new
         files.each do |filename|
-          source = File.read(filename)
           buffer = Parser::Source::Buffer.new filename
-          buffer.source = source
-          parser.reset
-          ast = parser.parse(buffer)
-          puts filename
+          buffer.read
+          ast = parser.reset.parse(buffer)
           processor.process(ast)
         end
       end
     end
 
     class Processor < Parser::AST::Processor
-      def on_const(node)
-        # require 'pry'
-        # require 'pp'
-        # binding.pry
-        # class_name = node.children.first.children.last
-        if node.children.last =~ /^[A-Z]+$/
-          puts node.inspect
-        end
+
+      # on_casgn: on constant assignment
+      def on_casgn(node)
+        expression = node.location.expression
+        filename = expression.source_buffer.name
+        puts "File: #{filename}"
+        puts expression.source
       end
+
     end
 
   end
