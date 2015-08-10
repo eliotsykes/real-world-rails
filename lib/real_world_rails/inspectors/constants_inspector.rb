a9f4fdd6284a667d6c7a735ad21175258ba50f2c
Eliot Sykes <eliotsykes@gmail.com>
2015-08-10 15:03:22 +0100

    ConstantsInspector now excludes troublesome files

--- lib/real_world_rails/inspectors/constants_inspector.rb
+++ lib/real_world_rails/inspectors/constants_inspector.rb
@@ -1,18 +1,25 @@
+require_relative 'file_inspector'
+
 module RealWorldRails
   module Inspectors
 
     class ConstantsInspector
       def run
         parser = ParserFactory.create
-        # files = Dir.glob("apps/**/*.rb")
-        files = Dir.glob("apps/24pullrequests/app/**/*.rb")
+        files = Dir.glob("apps/**/**/*.rb")
         processor = Processor.new
         files.each do |filename|
-          buffer = Parser::Source::Buffer.new filename
-          buffer.read
-          ast = parser.reset.parse(buffer)
-          processor.process(ast)
+          if inspectable?(filename)
+            buffer = Parser::Source::Buffer.new filename
+            buffer.read
+            ast = parser.reset.parse(buffer)
+            processor.process(ast)
+          end
         end
+      end
+
+      def inspectable?(filename)
+        !FileInspector.from_gem?(filename) && !FileInspector.test?(filename) && !FileInspector.from_generator?(filename)
       end
     end
 
