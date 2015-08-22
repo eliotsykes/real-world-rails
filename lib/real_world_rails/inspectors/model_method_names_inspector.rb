2bff75a5d23f4b388ab2efd5bed173e22c5b912b
Eliot Sykes <eliotsykes@gmail.com>
2015-08-22 12:04:53 +0100

    Model method names inspector

---
+++ lib/real_world_rails/inspectors/model_method_names_inspector.rb
@@ -0,0 +1,20 @@
+module RealWorldRails
+  module Inspectors
+
+    class ModelMethodNamesInspector < Inspector
+
+      inspects :models
+
+      class Processor < Parser::AST::Processor
+        def on_def(node)
+          method_name = node.children.first
+          expression = node.location.expression
+          filename = expression.source_buffer.name
+          puts "#{method_name} (#{filename})"
+        end
+      end
+
+    end
+
+  end
+end
